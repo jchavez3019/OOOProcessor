9ead0ce8199b1c382a58fee9ee41b871d036f2b1
ssbonde2 <ssbonde2@illinois.edu>
2022-10-30 20:30:16 -0500

    regfile first draft

--- mp4/hdl/regfile.sv
+++ mp4/hdl/regfile.sv
@@ -4,13 +4,17 @@ module regfile
     input clk,
     input rst,
     input load,
+    input allocate,
     input [31:0] in,
     input [4:0] src_a, src_b, dest,
+    input [2:0] tag_in,
     output logic [31:0] reg_a, reg_b
 );
 
 //logic [31:0] data [32] /* synthesis ramstyle = "logic" */ = '{default:'0};
 logic [31:0] data [32];
+logic [2:0] tag [32];
+logic valid [32];       // 0 means waiting for ROB to fill it up, 1 means its rdy
 
 always_ff @(posedge clk)
 begin
@@ -18,12 +22,22 @@ begin
     begin
         for (int i=0; i<32; i=i+1) begin
             data[i] <= '0;
+            tag[i] <= '0;
+            valid[i] = 1
         end
     end
     else if (load && dest)
     begin
         data[dest] <= in;
+        valid[dest] <= 1;
     end
+
+    else if(allocate)
+    begin
+        valid[dest] <= 0
+        tag[dest] <= tag_in
+    end 
+
 end
 
 always_comb
