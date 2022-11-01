9bbec5e4ee932af199e5012679ec1ba69e068d12
ssbonde2 <ssbonde2@illinois.edu>
2022-10-31 20:26:45 -0500

    new cdb implementation

--- mp4/hdl/cdb.sv
+++ mp4/hdl/cdb.sv
@@ -1,88 +1,64 @@
-module cdb_latch (  
-                    input tomasula_types::ctl_word control1,
-                    input en1,
-                    input rstn1,
-                    output tomasula_types::ctl_word q1,
-                    input tomasula_types::ctl_word control2,
-                    input en2,
-                    input rstn2,
-                    output tomasula_types::ctl_word q2,
-                    input tomasula_types::ctl_word control3,
-                    input en3,
-                    input rstn3,
-                    output tomasula_types::ctl_word q3,
-                    input tomasula_types::ctl_word control4,
-                    input en4,
-                    input rstn4,
-                    output tomasula_types::ctl_word q4,
-                    input tomasula_types::ctl_word control5,
-                    input en5,
-                    input rstn5,
-                    output tomasula_types::ctl_word q5,
-                    input tomasula_types::ctl_word control6,
-                    input en6,
-                    input rstn6,
-                    output tomasula_types::ctl_word q6,
-                    input tomasula_types::ctl_word control7,
-                    input en7,
-                    input rstn7,
-                    output tomasula_types::ctl_word q7,
-                    input tomasula_types::ctl_word control8,
-                    input en8,
-                    input rstn8,
-                    output tomasula_types::ctl_word q8,
+module cdb (  input tomasula_types::ctl_word ctl [8],
+                    input enable [8],
+                    input rst [8],
+                    output tomasula_types::ctl_word out [8],
                     );
 
-always @ (en1 | rstn1 | ctl_word1) begin
-    if(!rst1) begin
-        q1 <= 0;
-    else
-        if(en1)
-            q1 <= d1;
-    end
-    if(!rst2) begin
-        q2 <= 0;
-    else
-        if(en2)
-            q2 <= d2;
-    end
+    cdb_latch latch0(
+        .control(ctl[0]),
+        .en(enable[0]),
+        .rstn(rst[0]),
+        .q(out[0])
+    );
 
-    if(!rst3) begin
-        q3 <= 0;
-    else
-        if(en3)
-            q3 <= d3;
-    end
-    if(!rst4) begin
-        q4 <= 0;
-    else
-        if(en4)
-            q4 <= d4;
-    end
-    if(!rst5) begin
-        q5 <= 0;
-    else
-        if(en5)
-            q5 <= d5;
-    end
-    if(!rst6) begin
-        q6 <= 0;
-    else
-        if(en6)
-            q6 <= d6;
-    end
-    if(!rst7) begin
-        q7 <= 0;
-    else
-        if(en7)
-            q7 <= d7;
-    end
-    if(!rst8) begin
-        q8 <= 0;
-    else
-        if(en8)
-            q8 <= d8;
-    end
-end
+    cdb_latch latch1(
+        .control(ctl[1]),
+        .en(enable[1]),
+        .rstn(rst[1]),
+        .q(out[1])
+    );
+
+    cdb_latch latch2(
+        .control(ctl[2]),
+        .en(enable[2]),
+        .rstn(rst[2]),
+        .q(out[2])
+    );
+
+    cdb_latch latch3()
+        .control(ctl[3]),
+        .en(enable[3]),
+        .rstn(rst[3]),
+        .q(out[3])
+    );
+
+    cdb_latch latch4(
+        .control(ctl[4]),
+        .en(enable[4]),
+        .rstn(rst[4]),
+        .q(out[4])
+    );
+
+    cdb_latch latch5(
+        .control(ctl[5]),
+        .en(enable[5]),
+        .rstn(rst[5]),
+        .q(out[5])
+    );
+
+    cdb_latch latch6(
+        .control(ctl[6]),
+        .en(enable[6]),
+        .rstn(rst[6]),
+        .q(out[6])
+    );
+
+    cdb_latch latch7(
+        .control(ctl[7]),
+        .en(enable[7]),
+        .rstn(rst[7]),
+        .q(out[7])
+    );
+
 
 endmodule
