abfa9ab2e4e7d2eabe73c02b48b3b3b1f9c51c77
jiwonl4 <jiwonl4@illinois.edu>
2022-11-08 22:07:37 -0600

    rob comments added

--- mp4/hdl/rob.sv
+++ mp4/hdl/rob.sv
@@ -53,9 +53,8 @@ logic _data_read, _data_write;
 logic _regfile_load;
 logic _rob_full;
 
-logic [2:0] _curr_ptr, _head_ptr, br_ptr;
+logic [2:0] _curr_ptr, _head_ptr, _br_ptr;
 
-// assign rd_commit = _rd_commit;
 assign rd_commit = rd_arr[_head_ptr];
 assign st_src_commit = _st_src_commit;
 assign ld_commit_sel = _ld_commit_sel;
@@ -66,6 +65,7 @@ assign regfile_load = _regfile_load;
 assign rob_full = _rob_full;
 assign curr_ptr = _curr_ptr;
 assign head_ptr = _head_ptr;
+assign br_ptr = _br_ptr;
 
 assign _rob_full = _head_ptr + 3'h7 == _curr_ptr;
 
@@ -79,9 +79,6 @@ assign status_rob_valid[6] = valid_arr[6];
 assign status_rob_valid[7] = valid_arr[7];
 
 always_ff @(posedge clk) begin
-    // _ld_br <= 1'b0;
-    // _ld_commit_sel <= 1'b0;
-    // _regfile_load <= 1'b0;
 
     if (rst) begin
         for (int i=0; i<8; i++) begin
@@ -91,7 +88,7 @@ always_ff @(posedge clk) begin
         end
         _curr_ptr <= 3'b000;
         _head_ptr <= 3'b000;
-        br_ptr <= 3'b000;
+        _br_ptr <= 3'b000;
         flush_ip <= 1'b0;
     end
 
@@ -101,94 +98,86 @@ always_ff @(posedge clk) begin
                 valid_arr[i] <= 1'b1;
             end
         end
+
+        /* ----- ALLOCATE -----*/
         if (rob_load) begin
-           // allocate ROB entry 
+           // stored to handle memory and branching
            instr_arr[_curr_ptr] <= instr_type; 
-           rd_arr[_curr_ptr] <= rd; 
-           // do not allocate regfile entry for st
+
+           // store - need to save register that holds data
            if (instr_type == tomasula_types::ST) begin 
                rd_arr[_curr_ptr] <= st_src;
            end
-           else if (instr_type != tomasula_types::BRANCH) begin
-               // output to regfile
-            //    _rd_commit <= rd;
+           // branch - hold taken/not taken (initialized to not taken)
+           else if (instr_type == tomasula_types::BRANCH) begin 
+               rd_arr[_curr_ptr] <= 3'b000;
            end
-           // increment _curr_ptr
-           //TODO: beware! overflow may cause errors
+           // all other instructions
+           else begin
+               rd_arr[_curr_ptr] <= rd; 
+           end
            _curr_ptr <= _curr_ptr + 1'b1;
         end
 
+        /* ----- COMMIT ----- */
         // if the head of the rob has been computed
         if (valid_arr[_head_ptr]) begin
-            if(instr_arr[_head_ptr] == tomasula_types::BRANCH) begin
-            //    _ld_br <= 1'b1; 
-            end
-            else if (instr_arr[_head_ptr] == tomasula_types::LD) begin
-                // _data_read <= 1'b1;
+            // load
+            if (instr_arr[_head_ptr] == tomasula_types::LD) begin
                 // make sure instruction is not committed until data returned
                 // from d-cache...
                 if (data_mem_resp) begin
-                    // _data_read <= 1'b0;
                     valid_arr[_head_ptr] <= 1'b0;
-                    // use d-cache data
-                    // _ld_commit_sel <= 1'b1;
-                    // _regfile_load <= 1'b1;
-                    // _rd_commit <= rd_arr[_head_ptr];
-                    // update head
                     _head_ptr <= _head_ptr + 1'b1;
                 end
             end
+            // store
             else if (instr_arr[_head_ptr] == tomasula_types::ST) begin
-                // _data_write <= 1'b1;
-                // for st address
                 // send regfile the register file to read from
                 _st_src_commit <= rd_arr[_head_ptr];
                 // once store has been processed
                 if (data_mem_resp) begin
-                    // _data_write <= 1'b0;
                     valid_arr[_head_ptr] <= 1'b0;
                     _head_ptr <= _head_ptr + 1'b1;
                 end
             end
-            // for all other instructions
+            // all other instructions
             else begin
-                // _regfile_load <= 1'b1;
                 valid_arr[_head_ptr] <= 1'b0;
-
-                // increment _head_ptr
-                // _rd_commit <= rd_arr[_head_ptr];
                 _head_ptr <= _head_ptr + 1'b1;
             end
         end
-        // branch mispredict handling
-        if (branch_mispredict) begin
-            flush_ip <= 1'b1;
-            br_ptr <= _head_ptr;
-        end 
-        // FIXME: does this if get processed on the same cycle flush_ip is set?
-        if (flush_ip) begin
-            if (instr_arr[br_ptr] == tomasula_types::BRANCH) begin
-                // if we reached the branch, set the valid bit. 
-                // will be committed on the next cycle
-                // FIXME: im dont think the valid bit needs to be set here,
-                // since at this point there was a branch already calculated
-                // which has the valid bit set already
-                valid_arr[br_ptr] = 1'b1;
-                // flush all instructions after the branch
-                for (int i = br_ptr + 1; i <= (_head_ptr + 7) % 8; i++) begin
-                    valid_arr[i] <= 1'b0;
-                end
-                // update current pointer
-                _curr_ptr = br_ptr;
-                // flush now finished processing
-                flush_ip <= 1'b0;
-            end
-            // if we haven't reached the branch yet
-            else begin
-                // _rd_commit <= rd_arr[br_ptr];
-                br_ptr <= br_ptr + 1'b1;
-            end
+    end
+end
+
+/* ----- BRANCH MISPREDICT ----- */  
+always_ff @(posedge clk) begin
+if (branch_mispredict) begin
+    flush_ip <= 1'b1;
+    _br_ptr <= _head_ptr;
+end 
+// FIXME: does this if get processed on the same cycle flush_ip is set?
+if (flush_ip) begin
+    if (instr_arr[_br_ptr] == tomasula_types::BRANCH) begin
+        // if we reached the branch, set the valid bit. 
+        // will be committed on the next cycle
+        // FIXME: im dont think the valid bit needs to be set here,
+        // since at this point there was a branch already calculated
+        // which has the valid bit set already
+        valid_arr[_br_ptr] = 1'b1;
+        // flush all instructions after the branch
+        for (int i = _br_ptr + 1; i <= (_head_ptr + 7) % 8; i++) begin
+            valid_arr[i] <= 1'b0;
         end
+        // update current pointer
+        _curr_ptr = _br_ptr;
+        // flush now finished processing
+        flush_ip <= 1'b0;
+    end
+    // if we haven't reached the branch yet
+    else begin
+        // _rd_commit <= rd_arr[br_ptr];
+        _br_ptr <= _br_ptr + 1'b1;
     end
 end
 
